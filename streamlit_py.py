6a8811718d771eeee06f4b478b3d29bdcd85ee54
fareedahokunade <90572113+fareedahokunade@users.noreply.github.com>
2023-12-07 15:48:30 +0200

    Update streamlit_py.py

--- streamlit_py.py
+++ streamlit_py.py
@@ -1,46 +1,48 @@
 import tensorflow
-import flask
 
-from flask import Flask, render_template, request, jsonify
+# Import necessary libraries
+import streamlit as st
+import pandas as pd
 from tensorflow.keras.models import model_from_json
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-app = Flask(__name__)
-
-# Load the model architecture from JSON
+# Load the trained model architecture from JSON
 with open('model_architecture.json', 'r') as json_file:
     loaded_model_json = json_file.read()
 
-# Create model from JSON
+# Create the model from JSON
 loaded_model = model_from_json(loaded_model_json)
 
 # Load the model weights
 loaded_model.load_weights('model_weights.h5')
 
-# Load tokenizer
-# Note: You may need to adjust this based on your preprocessing steps
+# Load tokenizer (adjust based on your preprocessing steps)
 tokenizer = Tokenizer()
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    title = request.form['title']
-    text = request.form['text']
-
-    # Preprocess the input (adjust based on your preprocessing steps)
+# Function to preprocess and predict
+def predict_fake_news(title, text):
     input_text = title.lower() + ' ' + text.lower()
     input_seq = tokenizer.texts_to_sequences([input_text])
     input_pad = pad_sequences(input_seq, maxlen=200, padding='post')
-
-    # Make predictions
     prediction = loaded_model.predict(input_pad)[0][0]
+    return prediction
 
-    # Return the result
-    return render_template('result.html', prediction=prediction)
+# Streamlit app
+def main():
+    st.title("Fake News Detection App")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    # Input fields for user to enter title and text
+    title = st.text_input("Enter the title:")
+    text = st.text_area("Enter the text:")
+
+    # Button to trigger prediction
+    if st.button("Predict"):
+        # Perform prediction
+        prediction = predict_fake_news(title, text)
+
+        # Display result
+        st.write(f"Prediction: {prediction:.2%} chance of being fake news")
+
+if __name__ == "__main__":
+    main()
