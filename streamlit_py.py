59da23a9e73ed93bfa9e120a4efef3daa9bce066
fareedahokunade <90572113+fareedahokunade@users.noreply.github.com>
2023-12-07 17:10:46 +0200

    Update streamlit_py.py

--- streamlit_py.py
+++ streamlit_py.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-from tensorflow.keras.models import load_model
 from tensorflow.keras.models import model_from_json
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
@@ -10,44 +9,59 @@ from nltk.stem import WordNetLemmatizer
 from nltk import pos_tag
 import string
 from sklearn.preprocessing import LabelEncoder
+
 nltk.download('punkt')
 nltk.download('wordnet')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('vader_lexicon')
 nltk.download("stopwords")
 
+# Load the pre-trained model architecture from a JSON file
+with open('path/to/your/model/architecture.json', 'r') as json_file:
+    loaded_model_json = json_file.read()
+
+# Load the pre-trained model weights
+model_weights_file = 'path/to/your/model/weights.h5'
+
 # Load the pre-trained model
-with open('model_architecture.json', 'r') as json_file:
-    model_json = json_file.read()
-
-model = model_from_json(model_json)
-
-# Load your model weights
-model.load_weights('model_weights.h5')
-
+model = model_from_json(loaded_model_json)
+model.load_weights(model_weights_file)
 
 # Load other necessary components like tokenizer and label encoder
 # Make sure to save these components during training and load them here
+max_words = 10000
+tokenizer = Tokenizer(num_words=max_words)
+tokenizer.fit_on_texts([""])  # Empty fit to avoid errors
 
-def preprocess_text(text):
+def preprocess_text(title, text):
     # Your pre-processing steps here
+    title = title.lower()
     text = text.lower()
+    
+    title = ''.join(ch for ch in title if ch not in string.punctuation)
     text = ''.join(ch for ch in text if ch not in string.punctuation)
-    tokens = word_tokenize(text)
-    tokens = [token for token in tokens if token not in nltk.corpus.stopwords.words('english')]
-    tokens = [WordNetLemmatizer().lemmatize(token) for token in tokens]
-    pos_tags = pos_tag(tokens)
+    
+    title_tokens = word_tokenize(title)
+    text_tokens = word_tokenize(text)
+    
+    title_tokens = [token for token in title_tokens if token not in nltk.corpus.stopwords.words('english')]
+    text_tokens = [token for token in text_tokens if token not in nltk.corpus.stopwords.words('english')]
+    
+    title_tokens = [WordNetLemmatizer().lemmatize(token) for token in title_tokens]
+    text_tokens = [WordNetLemmatizer().lemmatize(token) for token in text_tokens]
+    
+    title_pos_tags = pos_tag(title_tokens)
+    text_pos_tags = pos_tag(text_tokens)
+    
     # Add more pre-processing steps as needed
-    return ' '.join(tokens)
+    
+    return ' '.join(title_tokens + text_tokens)
 
-def predict_fake_news(text):
-    # Pre-process the input text
-    processed_text = preprocess_text(text)
+def predict_fake_news(title, text):
+    # Pre-process the input title and text
+    processed_text = preprocess_text(title, text)
     
     # Tokenize and pad the sequence
-    max_words = 10000
-    tokenizer = Tokenizer(num_words=max_words)
-    tokenizer.fit_on_texts([processed_text])
     seq = tokenizer.texts_to_sequences([processed_text])
     padded_seq = pad_sequences(seq, maxlen=200, padding='post')
 
@@ -59,10 +73,17 @@ def predict_fake_news(text):
 # Streamlit UI
 st.title("Fake News Detection App")
 
-user_input = st.text_area("Enter the news text:")
+# Input text from the user
+title_input = st.text_input("Enter the news title:")
+text_input = st.text_area("Enter the news text:")
+
+# Button to trigger prediction
 if st.button("Predict"):
-    if user_input:
-        result = predict_fake_news(user_input)
-        st.write(f"The news has a {result*100:.2f}% chance of being fake.")
+    if title_input and text_input:
+        # Make prediction
+        prediction = predict_fake_news(title_input, text_input)
+
+        # Display the result
+        st.write(f"The news has a {prediction * 100:.2f}% chance of being fake.")
     else:
-        st.warning("Please enter some news text.")
+        st.warning("Please enter both news title and text.")
